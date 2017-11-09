99051caeca8b11d114d5a0f90188fd73a3ba6711
responsive kaysta <me@responsive-kaysta.ch>
2017-11-09 21:51:14 +0100

    Platform fix and Nunit project

---
+++ InMemoryLoaderBaseNunit/InMemoryLoaderBaseTestClass.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace InMemoryLoaderBaseNunit
+{
+	[TestFixture ()]
+	public class InMemoryLoaderBaseTestClass
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+		}
+	}
+}
+
