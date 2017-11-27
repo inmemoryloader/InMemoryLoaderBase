0b779cd687cc97271a8164dd5e5b1d4b18cddb75
Responsive Kaysta <me@responsive-kaysta.ch>
2017-11-27 08:28:14 +0100

    CodeInspection #4 (27 Nov 17 08:20)

--- InMemoryLoaderBase/HelperEnum/HashAlgorithmKind.cs
+++ InMemoryLoaderBase/HelperEnum/HashAlgorithmKind.cs
@@ -25,150 +25,150 @@
 
 using System;
 
-namespace InMemoryLoaderBase
+namespace InMemoryLoaderBase.HelperEnum
 {
-	/// <summary>
-	/// Aufzählung für die unterstützten Hashing-Algorithmen
-	/// </summary>
-	public enum HashAlgorithmKind
-	{
-		/// <summary>
-		/// MD5-Verfahren, das einen Hash mit einer Länge von 128 Bit erzeugt
-		/// </summary>
-		MD5,
+    /// <summary>
+    /// Aufzählung für die unterstützten Hashing-Algorithmen
+    /// </summary>
+    public enum HashAlgorithmKind
+    {
+        /// <summary>
+        /// MD5-Verfahren, das einen Hash mit einer Länge von 128 Bit erzeugt
+        /// </summary>
+        Md5,
 
-		/// <summary>
-		/// CNG-(Cryptography Next Generation)Implementierung des MD5-Verfahrens
-		/// </summary>
-		MD5Cng,
+        /// <summary>
+        /// CNG-(Cryptography Next Generation)Implementierung des MD5-Verfahrens
+        /// </summary>
+        Md5Cng,
 
-		/// <summary>
-		/// RIPEMD-Verfahren, das einen Hash mit einer Länge von 160 Bit erzeugt
-		/// </summary>
-		/// <remarks>
-		/// Entwickelt als Ersatz für MD4 und MD5
-		/// </remarks>
-		RIPEMD160,
+        /// <summary>
+        /// RIPEMD-Verfahren, das einen Hash mit einer Länge von 160 Bit erzeugt
+        /// </summary>
+        /// <remarks>
+        /// Entwickelt als Ersatz für MD4 und MD5
+        /// </remarks>
+        RipeMd160,
 
-		/// <summary>
-		/// SHA-Verfahren, das einen Hash mit einer Länge von 160 Bit erzeugt
-		/// </summary>
-		SHA1,
+        /// <summary>
+        /// SHA-Verfahren, das einen Hash mit einer Länge von 160 Bit erzeugt
+        /// </summary>
+        Sha1,
 
-		/// <summary>
-		/// CNG (Cryptography Next Generation)-Implementierung des SHA-Verfahrens
-		/// </summary>
-		SHA1Cng,
+        /// <summary>
+        /// CNG (Cryptography Next Generation)-Implementierung des SHA-Verfahrens
+        /// </summary>
+        Sha1Cng,
 
-		/// <summary>
-		/// SHA-Verfahren, das einen Hash mit einer Länge von 256 Bit erzeugt
-		/// </summary>
-		SHA256,
+        /// <summary>
+        /// SHA-Verfahren, das einen Hash mit einer Länge von 256 Bit erzeugt
+        /// </summary>
+        Sha256,
 
-		/// <summary>
-		/// CNG (Cryptography Next Generation)-Implementierung des SHA256-Verfahrens
-		/// </summary>
-		SHA256Cng,
+        /// <summary>
+        /// CNG (Cryptography Next Generation)-Implementierung des SHA256-Verfahrens
+        /// </summary>
+        Sha256Cng,
 
-		/// <summary>
-		/// SHA-Verfahren, das einen Hash mit einer Länge von 384 Bit erzeugt
-		/// </summary>
-		SHA384,
+        /// <summary>
+        /// SHA-Verfahren, das einen Hash mit einer Länge von 384 Bit erzeugt
+        /// </summary>
+        Sha384,
 
-		/// <summary>
-		/// CNG (Cryptography Next Generation)-Implementierung des SHA384-Verfahrens
-		/// </summary>
-		SHA384Cng,
+        /// <summary>
+        /// CNG (Cryptography Next Generation)-Implementierung des SHA384-Verfahrens
+        /// </summary>
+        Sha384Cng,
 
-		/// <summary>
-		/// SHA-Verfahren, das einen Hash mit einer Länge von 512 Bit erzeugt
-		/// </summary>
-		SHA512,
+        /// <summary>
+        /// SHA-Verfahren, das einen Hash mit einer Länge von 512 Bit erzeugt
+        /// </summary>
+        Sha512,
 
-		/// <summary>
-		/// CNG (Cryptography Next Generation)-Implementierung des SHA512-Verfahrens
-		/// </summary>
-		SHA512Cng,
+        /// <summary>
+        /// CNG (Cryptography Next Generation)-Implementierung des SHA512-Verfahrens
+        /// </summary>
+        Sha512Cng,
 
-		/// <summary>
-		/// Hash-based Message Authentication Code (HMAC) über das RIPEMD160-Verfahren
-		/// </summary>
-		/// <remarks>
-		/// HMAC verwendet einen privaten Schlüssel, der mit den Daten vermischt wird.
-		/// Das Ergebnis wird über das Hashing-Verfahren in einem Hash umgewandelt,
-		/// der Hashcode wird wieder mit dem privaten Schlüssel vermengt und die
-		/// Hashfunktion wird ein zweites Mal auf diese Datenmenge angewendet.
-		/// HMACMD5 verwendet das MD5-Verfahren, das einen Hash der Länge 128 Bit erzeugt.
-		/// </remarks>
-		HMACMD5,
+        /// <summary>
+        /// Hash-based Message Authentication Code (HMAC) über das RIPEMD160-Verfahren
+        /// </summary>
+        /// <remarks>
+        /// HMAC verwendet einen privaten Schlüssel, der mit den Daten vermischt wird.
+        /// Das Ergebnis wird über das Hashing-Verfahren in einem Hash umgewandelt,
+        /// der Hashcode wird wieder mit dem privaten Schlüssel vermengt und die
+        /// Hashfunktion wird ein zweites Mal auf diese Datenmenge angewendet.
+        /// HMACMD5 verwendet das MD5-Verfahren, das einen Hash der Länge 128 Bit erzeugt.
+        /// </remarks>
+        HmacMd5,
 
-		/// <summary>
-		/// Hash-based Message Authentication Code (HMAC) über das RIPEMD160-Verfahren
-		/// </summary>
-		/// <remarks>
-		/// HMAC verwendet einen privaten Schlüssel, der mit den Daten vermischt wird.
-		/// Das Ergebnis wird über das Hashing-Verfahren in einem Hash umgewandelt,
-		/// der Hashcode wird wieder mit dem privaten Schlüssel vermengt und die
-		/// Hashfunktion wird ein zweites Mal auf diese Datenmenge angewendet.
-		/// HMACRIPEMD160 verwendet das RIPEMD160-Verfahren, das einen Hash der Länge 160 Bit erzeugt.
-		/// </remarks>
-		HMACRIPEMD160,
+        /// <summary>
+        /// Hash-based Message Authentication Code (HMAC) über das RIPEMD160-Verfahren
+        /// </summary>
+        /// <remarks>
+        /// HMAC verwendet einen privaten Schlüssel, der mit den Daten vermischt wird.
+        /// Das Ergebnis wird über das Hashing-Verfahren in einem Hash umgewandelt,
+        /// der Hashcode wird wieder mit dem privaten Schlüssel vermengt und die
+        /// Hashfunktion wird ein zweites Mal auf diese Datenmenge angewendet.
+        /// HMACRIPEMD160 verwendet das RIPEMD160-Verfahren, das einen Hash der Länge 160 Bit erzeugt.
+        /// </remarks>
+        HmacRipemd160,
 
-		/// <summary>
-		/// Hash-based Message Authentication Code (HMAC) über das SHA1-Verfahren
-		/// </summary>
-		/// <remarks>
-		/// HMAC verwendet einen privaten Schlüssel, der mit den Daten vermischt wird.
-		/// Das Ergebnis wird über das Hashing-Verfahren in einem Hash umgewandelt,
-		/// der Hashcode wird wieder mit dem privaten Schlüssel vermengt und die
-		/// Hashfunktion wird ein zweites Mal auf diese Datenmenge angewendet.
-		/// HMACSHA1 verwendet das SHA1-Verfahren, das einen Hash der Länge 160 Bit erzeugt.
-		/// </remarks>
-		HMACSHA1,
+        /// <summary>
+        /// Hash-based Message Authentication Code (HMAC) über das SHA1-Verfahren
+        /// </summary>
+        /// <remarks>
+        /// HMAC verwendet einen privaten Schlüssel, der mit den Daten vermischt wird.
+        /// Das Ergebnis wird über das Hashing-Verfahren in einem Hash umgewandelt,
+        /// der Hashcode wird wieder mit dem privaten Schlüssel vermengt und die
+        /// Hashfunktion wird ein zweites Mal auf diese Datenmenge angewendet.
+        /// HMACSHA1 verwendet das SHA1-Verfahren, das einen Hash der Länge 160 Bit erzeugt.
+        /// </remarks>
+        HmacSha1,
 
-		/// <summary>
-		/// Hash-based Message Authentication Code (HMAC) über das SHA256-Verfahren
-		/// </summary>
-		/// <remarks>
-		/// HMAC verwendet einen privaten Schlüssel, der mit den Daten vermischt wird.
-		/// Das Ergebnis wird über das Hashing-Verfahren in einem Hash umgewandelt,
-		/// der Hashcode wird wieder mit dem privaten Schlüssel vermengt und die
-		/// Hashfunktion wird ein zweites Mal auf diese Datenmenge angewendet.
-		/// HMACSHA256 verwendet das SHA1-Verfahren, das einen Hash der Länge 256 Bit erzeugt.
-		/// </remarks>
-		HMACSHA256,
+        /// <summary>
+        /// Hash-based Message Authentication Code (HMAC) über das SHA256-Verfahren
+        /// </summary>
+        /// <remarks>
+        /// HMAC verwendet einen privaten Schlüssel, der mit den Daten vermischt wird.
+        /// Das Ergebnis wird über das Hashing-Verfahren in einem Hash umgewandelt,
+        /// der Hashcode wird wieder mit dem privaten Schlüssel vermengt und die
+        /// Hashfunktion wird ein zweites Mal auf diese Datenmenge angewendet.
+        /// HMACSHA256 verwendet das SHA1-Verfahren, das einen Hash der Länge 256 Bit erzeugt.
+        /// </remarks>
+        HmacSha256,
 
-		/// <summary>
-		/// Hash-based Message Authentication Code (HMAC) über das SHA384-Verfahren
-		/// </summary>
-		/// <remarks>
-		/// HMAC verwendet einen privaten Schlüssel, der mit den Daten vermischt wird.
-		/// Das Ergebnis wird über das Hashing-Verfahren in einem Hash umgewandelt,
-		/// der Hashcode wird wieder mit dem privaten Schlüssel vermengt und die
-		/// Hashfunktion wird ein zweites Mal auf diese Datenmenge angewendet.
-		/// HMACSHA384 verwendet das SHA1-Verfahren, das einen Hash der Länge 384 Bit erzeugt.
-		/// </remarks>
-		HMACSHA384,
+        /// <summary>
+        /// Hash-based Message Authentication Code (HMAC) über das SHA384-Verfahren
+        /// </summary>
+        /// <remarks>
+        /// HMAC verwendet einen privaten Schlüssel, der mit den Daten vermischt wird.
+        /// Das Ergebnis wird über das Hashing-Verfahren in einem Hash umgewandelt,
+        /// der Hashcode wird wieder mit dem privaten Schlüssel vermengt und die
+        /// Hashfunktion wird ein zweites Mal auf diese Datenmenge angewendet.
+        /// HMACSHA384 verwendet das SHA1-Verfahren, das einen Hash der Länge 384 Bit erzeugt.
+        /// </remarks>
+        HmacSha384,
 
-		/// <summary>
-		/// Hash-based Message Authentication Code (HMAC) über das SHA512-Verfahren
-		/// </summary>
-		/// <remarks>
-		/// HMAC verwendet einen privaten Schlüssel, der mit den Daten vermischt wird.
-		/// Das Ergebnis wird über das Hashing-Verfahren in einem Hash umgewandelt,
-		/// der Hashcode wird wieder mit dem privaten Schlüssel vermengt und die
-		/// Hashfunktion wird ein zweites Mal auf diese Datenmenge angewendet.
-		/// HMACSHA512 verwendet das SHA1-Verfahren, das einen Hash der Länge 512 Bit erzeugt.
-		/// </remarks>
-		HMACSHA512,
+        /// <summary>
+        /// Hash-based Message Authentication Code (HMAC) über das SHA512-Verfahren
+        /// </summary>
+        /// <remarks>
+        /// HMAC verwendet einen privaten Schlüssel, der mit den Daten vermischt wird.
+        /// Das Ergebnis wird über das Hashing-Verfahren in einem Hash umgewandelt,
+        /// der Hashcode wird wieder mit dem privaten Schlüssel vermengt und die
+        /// Hashfunktion wird ein zweites Mal auf diese Datenmenge angewendet.
+        /// HMACSHA512 verwendet das SHA1-Verfahren, das einen Hash der Länge 512 Bit erzeugt.
+        /// </remarks>
+        HmacSha512,
 
-		/// <summary>
-		/// Message Authentication Code (MAC) über das TripleDES-Verfahren
-		/// </summary>
-		/// <remarks>
-		/// MACTripleDES setzt das TripleDES-Verfahren ein und erzeugt eunen Hash von 
-		/// 64 Bit Länge
-		/// </remarks>
-		MACTripleDES
-	}
+        /// <summary>
+        /// Message Authentication Code (MAC) über das TripleDES-Verfahren
+        /// </summary>
+        /// <remarks>
+        /// MACTripleDES setzt das TripleDES-Verfahren ein und erzeugt eunen Hash von 
+        /// 64 Bit Länge
+        /// </remarks>
+        MacTripleDes
+    }
 }
