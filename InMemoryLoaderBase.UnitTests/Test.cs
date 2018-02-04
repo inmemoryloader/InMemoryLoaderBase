19fd0969ddf7b1ba43aff83ac73f0e539dd7cec2
Kay Stuckenschmidt <responsive-it@outlook.com>
2018-02-04 09:39:02 +0000

    add new UnitTests

---
+++ InMemoryLoaderBase.UnitTests/Test.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+namespace InMemoryLoaderBase.UnitTests
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+
+            var key = AbstractComponent.Key;
+
+
+            Assert.IsNotNull(key);
+        }
+    }
+}
