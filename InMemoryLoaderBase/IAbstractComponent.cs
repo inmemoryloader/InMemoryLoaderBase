c077033f602e18862181e9572e584878308c2ac7
Kay Stuckenschmidt <responsive-it@outlook.com>
2019-02-08 17:41:37 +0000

    new header added

--- InMemoryLoaderBase/IAbstractComponent.cs
+++ InMemoryLoaderBase/IAbstractComponent.cs
@@ -1,8 +1,9 @@
-﻿// -------------------------------------------------------------------------------------------------------------------
-// Filename: IAbstractComponent.cs
-// Author: Kay Stuckenschmidt
+﻿//
+// IAbstractComponent.cs
 //
-// Copyright (c) 2019 responsive IT
+// Author: Kay Stuckenschmidt <dev-guru@responsive-it.biz>
+//
+// Copyright (c) 2010 responsive IT
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -21,7 +22,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-// -------------------------------------------------------------------------------------------------------------------
+//
 
 namespace InMemoryLoaderBase 
 {
